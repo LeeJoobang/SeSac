28b8ab85348b0688debd0aa5f0e394439fbe8a00
Joobang <hii5074@gmail.com>
2022-07-05 23:22:56 +0900

    [update] 영화 랜덤시 중복오류 처리

--- Movie/Movie/ViewController.swift
+++ Movie/Movie/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController{
-   
+    
     @IBOutlet weak var posterImage: UIImageView!
     @IBOutlet weak var firstImage: UIImageView!
     @IBOutlet weak var secondImage: UIImageView!
@@ -9,22 +9,34 @@ class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        posterImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-        firstImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-        secondImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-        thirdImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-  
-        firstImage.layer.cornerRadius = 0.5 * firstImage.bounds.size.width
-        secondImage.layer.cornerRadius = 0.5 * firstImage.bounds.size.width
-        thirdImage.layer.cornerRadius = 0.5 * firstImage.bounds.size.width
-
+        randomImage()
+        imageRound()
+        
     }
     
     @IBAction func changeButton(_ sender: Any) {
-        posterImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-        firstImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-        secondImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
-        thirdImage.image = UIImage(named: "movie\(Int.random(in: 1...20))")
+        randomImage()
     }
+    
+    func randomNumberArr()->[Int]{
+        let movieNum = Array(1...20)
+        let shuffleMoive = movieNum.shuffled()
+        return shuffleMoive
+    }
+    
+    func randomImage(){
+        let randomList = randomNumberArr()
+        posterImage.image = UIImage(named: "movie\(randomList[0])")
+        firstImage.image = UIImage(named: "movie\(randomList[1])")
+        secondImage.image = UIImage(named: "movie\(randomList[2])")
+        thirdImage.image = UIImage(named: "movie\(randomList[3])")
+    }
+    
+    func imageRound(){
+        firstImage.layer.cornerRadius = 0.5 * firstImage.bounds.size.width
+        secondImage.layer.cornerRadius = 0.5 * secondImage.bounds.size.width
+        thirdImage.layer.cornerRadius = 0.5 * thirdImage.bounds.size.width
+        
+    }
+    
 }
