f7e52d2746364bc8643b2a6f2f152751e1fb430a
Joobang <hii5074@gmail.com>
2022-07-05 17:09:23 +0900

    [update] 주석 삭제

--- Baedal/Baedal/ViewController.swift
+++ Baedal/Baedal/ViewController.swift
@@ -1,28 +1,10 @@
-//
-//  ViewController.swift
-//  Baedal
-//
-//  Created by Joobang Lee on 2022/07/04.
-//
-
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 16
-    }
+class ViewController: UIViewController {
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        return cell
-    }
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 }
 
