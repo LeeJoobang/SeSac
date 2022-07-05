d082cfa1c858e5f98a0fc8b0a273082c1648fc1e
Joobang <hii5074@gmail.com>
2022-07-05 17:37:44 +0900

    [update] image 추가

--- Baedal/Baedal/ViewController.swift
+++ Baedal/Baedal/ViewController.swift
@@ -2,8 +2,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var oneImage: UIImageView!
+    @IBOutlet weak var twoImage: UIImageView!
+    @IBOutlet weak var threeImage: UIImageView!
+    @IBOutlet weak var fourImage: UIImageView!
+    @IBOutlet weak var fiveImage: UIImageView!
+    @IBOutlet weak var sixImage: UIImageView!
+    @IBOutlet weak var sevenImage: UIImageView!
+    @IBOutlet weak var eightImage: UIImageView!
+    @IBOutlet weak var nineImage: UIImageView!
+    @IBOutlet weak var tenImage: UIImageView!
+    @IBOutlet weak var elevenImage: UIImageView!
+    @IBOutlet weak var twelveImage: UIImageView!
+    @IBOutlet weak var thirteenImage: UIImageView!
+    @IBOutlet weak var fourteenImage: UIImageView!
+    @IBOutlet weak var fifteenImage: UIImageView!
+    @IBOutlet weak var sixteenImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        oneImage.image = UIImage(named: "mono_baedal01")
+        twoImage.image = UIImage(named: "mono_baedal02")
+        threeImage.image = UIImage(named: "mono_baedal03")
+        fourImage.image = UIImage(named: "mono_baedal04")
+        fiveImage.image = UIImage(named: "mono_baedal05")
+        sixImage.image = UIImage(named: "mono_baedal06")
+        sevenImage.image = UIImage(named: "mono_baedal07")
+        eightImage.image = UIImage(named: "mono_baedal08")
+        nineImage.image = UIImage(named: "mono_baedal09")
+        tenImage.image = UIImage(named: "mono_baedal10")
+        elevenImage.image = UIImage(named: "mono_baedal11")
+        twelveImage.image = UIImage(named: "mono_baedal12")
+        thirteenImage.image = UIImage(named: "mono_baedal13")
+        fourteenImage.image = UIImage(named: "mono_baedal14")
+        fifteenImage.image = UIImage(named: "mono_baedal15")
+        sixteenImage.image = UIImage(named: "mono_baedal16")
+
         
     }
 }
