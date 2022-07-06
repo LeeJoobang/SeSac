41ff00bbf5edc36ac4a618990bdba30acbc76aca
Joobang <hii5074@gmail.com>
2022-07-06 17:17:45 +0900

    [add] title 작성

--- Movie/Movie/SignUpViewController.swift
+++ Movie/Movie/SignUpViewController.swift
@@ -2,9 +2,17 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nameLabel()
+    }
+    
+    func nameLabel(){
+        titleLabel.text = "JooFlix"
+        titleLabel.textColor = .red
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 50)
     }
     
 }
