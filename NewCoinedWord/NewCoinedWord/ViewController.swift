a82f75cc26de4aab7fa142ab7dc6f64e1c79976f
Joobang <hii5074@gmail.com>
2022-07-08 17:03:53 +0900

    [add] View 객체 Outlet 연결

--- NewCoinedWord/NewCoinedWord/ViewController.swift
+++ NewCoinedWord/NewCoinedWord/ViewController.swift
@@ -2,6 +2,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet var tagCollection: [UIButton]!
+    @IBOutlet weak var oneTagButton: UIButton!
+    @IBOutlet weak var twoTagButton: UIButton!
+    @IBOutlet weak var threeTagButton: UIButton!
+    @IBOutlet weak var fourTagButton: UIButton!
+    @IBOutlet weak var displayTextField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
