459a5a667ed3ac9903c8507512eaec57f25735c0
Joobang <hii5074@gmail.com>
2022-07-11 12:39:02 +0900

    [add] 오전 강의 따라하기

--- SeSac2Week/SeSac2Week/ViewController.swift
+++ SeSac2Week/SeSac2Week/ViewController.swift
@@ -5,9 +5,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var orangeView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        orangeView.layer.cornerRadius = 20
-        orangeView.clipsToBounds = false
+        orangeView.layer.maskedCorners = [.layerMaxXMaxYCorner] // 오 한쪽만 꺾인다!
+        orangeView.layer.cornerRadius = 10
+    }
+    
+    @IBAction func sliderViewChanged(_ sender: UISlider) {
+        print("sliderChanged")
+    }
+    
+    @IBAction func keyboardEnter(_ sender: UITextField) {
+        print("hi")
     }
 }
 
