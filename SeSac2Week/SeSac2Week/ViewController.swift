062281d663bacccd382f54e4dddc2a57702552cc
Joobang <hii5074@gmail.com>
2022-07-11 23:07:26 +0900

    [add] autolayout 전체 배경 표현

--- SeSac2Week/SeSac2Week/ViewController.swift
+++ SeSac2Week/SeSac2Week/ViewController.swift
@@ -2,19 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var orangeView: UIView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        orangeView.layer.maskedCorners = [.layerMaxXMaxYCorner] // 오 한쪽만 꺾인다!
-        orangeView.layer.cornerRadius = 10
+ 
     }
-    
-    @IBAction func sliderViewChanged(_ sender: UISlider) {
-        print("sliderChanged")
-    }
-    
-    @IBAction func keyboardEnter(_ sender: UITextField) {
-        print("hi")
-    }
+
 }
 
