91dc8354e080fe118d5392bb11a583e25a443497
Joobang <hii5074@gmail.com>
2022-07-11 09:48:24 +0900

    [delete] 주석 삭제

--- SeSac2Week/SeSac2Week/ViewController.swift
+++ SeSac2Week/SeSac2Week/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  SeSac2Week
-//
-//  Created by Joobang Lee on 2022/07/11.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,9 +5,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var orangeView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         orangeView.layer.cornerRadius = 20
-        
+        orangeView.clipsToBounds = false
     }
 }
 
