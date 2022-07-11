0d2b3c55935e564c7b98609494bb86c66ed01705
Joobang <hii5074@gmail.com>
2022-07-11 09:44:43 +0900

    test

--- SeSac2Week/SeSac2Week/ViewController.swift
+++ SeSac2Week/SeSac2Week/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orangeView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        orangeView.layer.cornerRadius = 20
+        
     }
-
-
 }
 
