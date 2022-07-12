0e05d7c4a70a3f7a8ea48873b5b8e475da524aa4
Joobang <hii5074@gmail.com>
2022-07-12 20:17:51 +0900

    [refactoring] enum + tag 활용하여 중복 상수 제거

--- NewCoinedWord/NewCoinedWord/ViewController.swift
+++ NewCoinedWord/NewCoinedWord/ViewController.swift
@@ -1,31 +1,39 @@
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+enum NewWord: String, CaseIterable {
+    case one = "쫌쫌다리"
+    case two = "억텐"
+    case three = "찐텐"
+    case four = "좋대구알"
+}
 
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var userView: UIView!
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
-    
     @IBOutlet var tagCollection: [UIButton]!
-    @IBOutlet weak var oneTagButton: UIButton!
-    @IBOutlet weak var twoTagButton: UIButton!
-    @IBOutlet weak var threeTagButton: UIButton!
-    @IBOutlet weak var fourTagButton: UIButton!
-    
     @IBOutlet weak var displayLabel: UILabel!
-       
+    
+    let meanArr = ["아주 조금씩 적고 하찮은 양을 모으는 모습", "억지 텐션", "진짜 텐션", "좋아요, 댓글, 구독, 알림설정"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         userTextField.delegate = self
-        
+        buttonTag()
         layout()
-        button()
+        buttonName()
         buttonlayout()
+    }
+    
+    func buttonTag(){
+        for item in 0...tagCollection.count - 1{
+            tagCollection[item].tag = item
+        }
     }
     
     func layout(){
         userView.backgroundColor = .black
-        
         searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
         searchButton.tintColor = .white
         searchButton.backgroundColor = .black
@@ -42,12 +50,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         displayLabel.numberOfLines = 0
     }
     
-    func button(){
+    func buttonName(){
+        let newWord = NewWord.allCases
+        print(newWord)
         var count = Int()
-        let buttonName = ["쫌쫌다리", "억텐", "찐텐", "좋대구알"]
-
         for hashItem in tagCollection{
-            hashItem.setTitle(buttonName[count], for: .normal)
+            hashItem.setTitle(newWord[count].rawValue, for: .normal)
             count += 1
         }
     }
@@ -61,34 +69,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return false
-    }
-    
-    
-    @IBAction func displayMean(_ sender: UIButton) {
-        let dic = [oneTagButton:"아주 조금씩 적고 하찮은 양을 모으는 모습", twoTagButton:"억지 텐션",threeTagButton:"진짜 텐션", fourTagButton:"좋아요, 댓글, 구독, 알림설정"]
-        
-        for item in dic {
-            let temp = sender.titleLabel?.text
-            let strItem = item.key?.titleLabel?.text
-            if temp == strItem {
-                displayLabel.text = item.value
-            }
-        }
+    @IBAction func touchedHashTagShowMean(_ sender: UIButton) {
+        displayLabel.text = meanArr[sender.tag]
     }
     
     @IBAction func searchWord(_ sender: UIButton) {
-        let dic = [oneTagButton:"아주 조금씩 적고 하찮은 양을 모으는 모습", twoTagButton:"억지 텐션",threeTagButton:"진짜 텐션", fourTagButton:"좋아요, 댓글, 구독, 알림설정"]
-        
-        for item in dic {
-            let temp = userTextField.text
-            let strItem = item.key?.titleLabel?.text
-            if temp == strItem {
-                displayLabel.text = item.value
-            }
+        let inputWord = userTextField.text ?? "nothing"
+        if inputWord == NewWord.one.rawValue {
+            displayLabel.text = meanArr[0]
+        } else if inputWord == NewWord.two.rawValue {
+            displayLabel.text = meanArr[1]
+        } else if inputWord == NewWord.three.rawValue {
+            displayLabel.text = meanArr[2]
+        } else if inputWord == NewWord.three.rawValue {
+            displayLabel.text = meanArr[3]
+        } else {
+            displayLabel.text = "오류"
         }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
     
     @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
