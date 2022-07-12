08dfe581a5d3d43a7aa684e393e50c3a6c27e31a
Joobang <hii5074@gmail.com>
2022-07-12 15:13:41 +0900

    [add] RawString 학습

---
+++ Playground/RawString.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// raw String 사용
+let menu: String = #"i love \kimchi"#
+let menu1: String = #"i love "kimchi"#
+
+print(menu)
+print(menu1)
+
+// 개행은 어떻게?
+
+let travel: String = #"i want to go \#nBerlin"#
+print(travel)
