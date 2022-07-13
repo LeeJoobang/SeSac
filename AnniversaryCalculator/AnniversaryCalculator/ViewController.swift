c8e0891a7239bfc58bf0f7c50b5917c20708d96c
Joobang <hii5074@gmail.com>
2022-07-13 14:31:26 +0900

    [delete] 주석 삭제

--- AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
+++ AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  AnniversaryCalculator
-//
-//  Created by Joobang Lee on 2022/07/13.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
