88d35fb3a8e71d3a0947fb2e5dcd01736c72346a
Joobang <hii5074@gmail.com>
2022-07-13 16:37:59 +0900

    [add] date String 추출

--- AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
+++ AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
@@ -2,10 +2,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userPickDate: UIDatePicker!
+    @IBOutlet var imageCollection: [UIImageView]!
+    @IBOutlet var dDayLabelCollection: [UILabel]!
+    @IBOutlet var thatDayCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func userWheelDate(_ sender: UIDatePicker) {
+        sender.datePickerMode = UIDatePicker.Mode.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        let selectedDate = dateFormatter.string(from: sender.date)
+        print(selectedDate)
+    }
+    
 
 }
 
