1b07172973fea9ce5570c43f96e5392a4eb22261
Joobang <hii5074@gmail.com>
2022-07-14 09:06:34 +0900

    [add] 유저 기간별 계산함수 추가

--- AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
+++ AnniversaryCalculator/AnniversaryCalculator/ViewController.swift
@@ -7,9 +7,12 @@ class ViewController: UIViewController {
     @IBOutlet var dDayLabelCollection: [UILabel]!
     @IBOutlet var thatDayCollection: [UILabel]!
     
+    var finalDate: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImage()
+        dDayLabel()
     }
     
     func backgroundImage(){
@@ -17,18 +20,43 @@ class ViewController: UIViewController {
             imageCollection[number].image = UIImage(named: "img\(number+1)")
             imageCollection[number].contentMode = .scaleToFill
             imageCollection[number].layer.cornerRadius = 20
-            imageCollection[number].alpha = 0.8
+            imageCollection[number].alpha = 0.3
         }
     }
     
-    @IBAction func userWheelDate(_ sender: UIDatePicker) {
-        sender.datePickerMode = UIDatePicker.Mode.date
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        let selectedDate = dateFormatter.string(from: sender.date)
-        print(selectedDate)
+    func dDayLabel(){
+        for number in 0...dDayLabelCollection.count - 1{
+            dDayLabelCollection[number].text = "D+\(number+1)00"
+        }
     }
     
+    func showCalculatedDate(_ sender: UIDatePicker){
+        for number in 0...thatDayCollection.count - 1{
+            thatDayCollection[number].text = finalDate
+            thatDayCollection[number].textAlignment = .center
+            thatDayCollection[number].textColor = .black
+        }
 
+    }
+    
+    
+    @IBAction func userWheelDate(_ sender: UIDatePicker) {
+      userPickDate(sender, plusDay: 100)
+      userPickDate(sender, plusDay: 200)
+      userPickDate(sender, plusDay: 300)
+      userPickDate(sender, plusDay: 400)    }
+    
+    func userPickDate(_ sender: UIDatePicker, plusDay day: Int){
+        sender.datePickerMode = UIDatePicker.Mode.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let selectedDate = dateFormatter.string(from: sender.date)
+        let startDate = dateFormatter.date(from: selectedDate)
+        let calculateDate = Calendar.current.date(byAdding: .day, value: day, to: startDate!)
+        finalDate = dateFormatter.string(from: calculateDate!)
+        print(finalDate)
+    }
+    
+    
 }
 
