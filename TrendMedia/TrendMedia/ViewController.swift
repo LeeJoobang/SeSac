e26cd535d651e773f6c2aced87e03e936acdd487
Joobang <hii5074@gmail.com>
2022-07-18 09:34:00 +0900

    [add] 주석삭제

--- TrendMedia/TrendMedia/ViewController.swift
+++ TrendMedia/TrendMedia/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  TrendMedia
-//
-//  Created by Joobang Lee on 2022/07/18.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
