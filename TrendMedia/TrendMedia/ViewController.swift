5cee6b405e00fc833493e79057d479c38a9f8c64
Joobang <hii5074@gmail.com>
2022-07-18 15:32:26 +0900

    [add] case 1 스토리 보드 생성

--- TrendMedia/TrendMedia/ViewController.swift
+++ TrendMedia/TrendMedia/ViewController.swift
@@ -2,8 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yellowViewLeadingContrstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        yellowViewLeadingContrstraint.constant = 120
+        
     }
 
 
