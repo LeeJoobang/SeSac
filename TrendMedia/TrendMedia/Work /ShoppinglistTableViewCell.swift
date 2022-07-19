ad385bb5e0bb036964b5f9dfec84ac058be0b145
Joobang <hii5074@gmail.com>
2022-07-19 17:41:09 +0900

    [feat] 셀 스와이프 후 삭제

--- TrendMedia/TrendMedia/Work /ShoppinglistTableViewCell.swift
+++ TrendMedia/TrendMedia/Work /ShoppinglistTableViewCell.swift
@@ -2,16 +2,27 @@ import UIKit
 
 class ShoppinglistTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var shoppingImageView: UIImageView!
+    @IBOutlet weak var checkboxImageView: UIImageView!
     @IBOutlet weak var shoppinglistLabel: UILabel!
-    @IBOutlet weak var checkboxButton: UIButton!
+    @IBOutlet weak var likeStarButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+         likeStarButton.setTitle("test", for: .normal)
+        // uibutton의 텍스트 변환이 가능하다. 그러나 이미지를 넣으려고 했으나, 반영이 안됨. 이유 확인 필요
+//        likeStarButton.setImage(UIImage(named: "star"), for: .normal)
+        
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
+    
+    @IBAction func likeStarButtonReturn(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+    }
+    
+    
+    
 
 }
--- TrendMedia/TrendMedia/Work /ShoppinglistTableViewController.swift
+++ TrendMedia/TrendMedia/Work /ShoppinglistTableViewController.swift
@@ -2,10 +2,18 @@ import UIKit
 
 class ShoppinglistTableViewController: UITableViewController {
     
+    @IBOutlet weak var shoppinglistLabel: UITextField!
+    
     var list = ["그립톡 구매하기", "사이다 구매", "아이패드 케이스 최저가 알아보기", "양말"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    @IBAction func shoppinglistReturn(_ sender: UITextField) {
+        list.append(sender.text!)
+        tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -17,9 +25,20 @@ class ShoppinglistTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppinglistTableViewCell", for: indexPath) as! ShoppinglistTableViewCell // as? 타입캐스팅
         cell.shoppinglistLabel.text = list[indexPath.row]
         cell.shoppinglistLabel.font = .boldSystemFont(ofSize: 18)
+        //cell.likeStarButton.setTitle("test", for: .normal)
+        
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        
+        if editingStyle == .delete{
+            list.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
 }
