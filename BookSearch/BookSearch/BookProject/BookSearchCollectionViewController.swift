a42f18efb645e3dbecb52c14e8333afaceab8fde
Joobang <hii5074@gmail.com>
2022-07-20 16:33:05 +0900

    [feat] collectionview cell 연결

---
+++ BookSearch/BookSearch/BookProject/BookSearchCollectionViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+class BookSearchCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookSearchCollectionViewCell", for: indexPath) as! BookSearchCollectionViewCell
+        
+        
+        return cell
+    }
+    
+}
