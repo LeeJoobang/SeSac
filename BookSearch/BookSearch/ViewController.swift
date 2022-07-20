2b880eec01168f3f39025ce60ec81d906337ffd9
Joobang <hii5074@gmail.com>
2022-07-20 16:21:44 +0900

    [add] 주석삭제

--- BookSearch/BookSearch/ViewController.swift
+++ BookSearch/BookSearch/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  BookSearch
-//
-//  Created by Joobang Lee on 2022/07/20.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
