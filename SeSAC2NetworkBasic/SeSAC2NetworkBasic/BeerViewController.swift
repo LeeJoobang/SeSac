3430ba035f18b6a0aa337b44d5c36525364355b1
Joobang <hii5074@gmail.com>
2022-08-01 16:24:03 +0900

    [feat] beer view layout

--- SeSAC2NetworkBasic/SeSAC2NetworkBasic/BeerViewController.swift
+++ SeSAC2NetworkBasic/SeSAC2NetworkBasic/BeerViewController.swift
@@ -5,8 +5,15 @@ import SwiftyJSON
 
 class BeerViewController: UIViewController {
 
+    @IBOutlet weak var beerImageView: UIImageView!
+    @IBOutlet weak var beerNameLabel: UILabel!
+    @IBOutlet weak var beerInformationLabel: UILabel!
+    @IBOutlet weak var randomButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
 }
