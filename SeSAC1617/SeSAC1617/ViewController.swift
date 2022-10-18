76af71f1aebbcc9d78a492476cd785c7e8469bc6
Joobang <hii507@gmail.com>
2022-10-18 09:45:47 +0900

    update

---
+++ SeSAC1617/SeSAC1617/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SeSAC1617
+//
+//  Created by Jooyoung Lee on 2022/10/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
