64258d6ea005c30f8e84f98215f0d63008b26ea9
Joobang <hii507@gmail.com>
2022-10-27 11:51:09 +0900

    update rxswift 심화

---
+++ SeSACWeek1617/SeSACWeek1617/Rx/ValidationViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ValidationViewController.swift
+//  SeSACWeek1617
+//
+//  Created by Jooyoung Lee on 2022/10/27.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ValidationViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    @IBOutlet weak var stepButton: UIButton!
+    
+    let disposeBag = DisposeBag()
+    let viewModel = ValidationViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bind()
+    }
+    
+    func bind(){
+        viewModel.validText
+            .asDriver()
+            .drive(validationLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let validataion = nameTextField.rx.text
+            .orEmpty // string
+            .map { $0.count >= 8 } // bool
+//            .share()
+        
+        validataion
+            .bind(to: stepButton.rx.isEnabled, validationLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        validataion
+            .withUnretained(self)
+            .bind { (vc, value) in
+                let color: UIColor = value ? .systemPink : .lightGray
+                vc.stepButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        stepButton.rx.tap
+            .bind { _ in
+                print("show Alert")
+            }
+            .disposed(by: disposeBag)
+        //stream == sequence
+//            .subscribe { _ in
+//                print("next")
+//            } onDisposed: {
+//                print("dispose")
+//            }
+//            .disposed(by: disposeBag) // dispose는 리소스 정리
+    }
+    
+    func observableVSSubject(){
+        let testA = stepButton.rx.tap
+            .map { "안녕하세요" }
+            .asDriver(onErrorJustReturn: "")
+        //            .share()
+        
+        testA
+            .drive(validationLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        testA
+            .drive(nameTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        testA
+            .drive(stepButton.rx.title())
+            .disposed(by: disposeBag)
+        
+        let sampleInt = Observable<Int>.create { observer in
+            observer.onNext(Int.random(in: 1...100))
+            return Disposables.create()
+        }
+        
+        sampleInt.subscribe { value in
+            print("SampleInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        sampleInt.subscribe { value in
+            print("SampleInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        sampleInt.subscribe { value in
+            print("SampleInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        
+        let subjectInt = BehaviorSubject(value: 0)
+        subjectInt.onNext(Int.random(in: 1...100))
+        
+        subjectInt.subscribe { value in
+            print("subjectInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        subjectInt.subscribe { value in
+            print("subjectInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        subjectInt.subscribe { value in
+            print("subjectInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+    }
+    
+    
+}
