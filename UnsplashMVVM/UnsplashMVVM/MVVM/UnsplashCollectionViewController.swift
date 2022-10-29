9bdf83deb0c9b33ace7956cb847a66ce9ee5be3d
Joobang <hii507@gmail.com>
2022-10-29 19:34:32 +0900

    [chore] 스토리 보드 연결

---
+++ UnsplashMVVM/UnsplashMVVM/MVVM/UnsplashCollectionViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class UnsplashCollectionViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
